dba75ab58ad43e2bcc48b5910f3e3d6eab4d1d0f
Kimpu <lionelkimpu@gmail.com>
2026-03-01 02:32:20 +0630

    add gzip compression and ETags for API performance

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import compression from "compression";
 import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import helmet from "helmet";
@@ -21,6 +22,7 @@ const app = express();
 export const CORS_ORIGIN = process.env.FRONTEND_URL || "http://localhost:3000";
 
 app.use(helmet());
+app.use(compression());
 app.use(express.json());
 app.use(
   cors({
